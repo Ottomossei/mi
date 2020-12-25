69c5e3b75f5fb9ab3b6fa2fabae1a9c8db5ebf21
Ottomossei <62988022+Ottomossei@users.noreply.github.com>
2020-12-26 00:45:08 +0900

    commit compname

---
+++ mi/compname.py
@@ -0,0 +1,15 @@
+import re
+test = ["LiNa2F5"]
+
+class CompName:
+    def __init__(self, path):
+        
+
+    def get_ratio(self, name):
+        print(name)
+
+
+if __name__ == "__main__":
+    cn = CompName()
+    test = ["LiNa2F5"]
+    cn.get_ratio(test)
